f7ee05c1ecfaeb0918f95267eb85fb40eb16ff4c
Riizlene <Rizlene.Belabdelli@etu.sorbonne-universite.fr>
2025-01-13 02:31:58 +0100

    Refactoring principe Clean Code

--- inventaire.c
+++ inventaire.c
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+// Affiche tous les articles disponibles dans l'inventaire
 void afficher_inventaire(const Inventaire* inventaire) {
     printf("Inventaire:\n");
     for (int i = 0; i < inventaire->taille; i++) {
@@ -13,6 +14,7 @@ void afficher_inventaire(const Inventaire* inventaire) {
     }
 }
 
+// Ajoute un article à l'inventaire ou on met à jour la quantité si l'article existe déjà
 void ajouter_article(Inventaire* inventaire, const char* nom, int quantite, float prix) {
     for (int i = 0; i < inventaire->taille; i++) {
         if (strcmp(inventaire->articles[i].nom, nom) == 0) {
@@ -20,12 +22,19 @@ void ajouter_article(Inventaire* inventaire, const char* nom, int quantite, floa
             return;
         }
     }
-    strcpy(inventaire->articles[inventaire->taille].nom, nom);
-    inventaire->articles[inventaire->taille].quantite = quantite;
-    inventaire->articles[inventaire->taille].prix = prix;
-    inventaire->taille++;
+
+    if (inventaire->taille < 100) { // Limite de stockage
+        strcpy(inventaire->articles[inventaire->taille].nom, nom);
+        inventaire->articles[inventaire->taille].quantite = quantite;
+        inventaire->articles[inventaire->taille].prix = prix;
+        inventaire->taille++;
+    }
+    else {
+        printf("L'inventaire est plein.\n");
+    }
 }
 
+// Retire une quantité d'un article de l'inventaire ou le supprime completement si la quantité est nulle
 void retirer_article(Inventaire* inventaire, const char* nom, int quantite) {
     for (int i = 0; i < inventaire->taille; i++) {
         if (strcmp(inventaire->articles[i].nom, nom) == 0) {
